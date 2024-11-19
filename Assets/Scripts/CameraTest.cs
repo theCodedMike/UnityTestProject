2af63efe07799a9ecd915d11c03f9781393cc2df
lixia <lixiaofeng1115@outlook.com>
2024-11-19 23:46:51 +0800

    复习11月19日的内容

--- Assets/Scripts/CameraTest.cs
+++ Assets/Scripts/CameraTest.cs
@@ -2,22 +2,71 @@ using UnityEngine;
 
 public class CameraTest : MonoBehaviour
 {
+    public Transform target;
+
+    public Transform targetUI;
+    
+    private Camera _mainCamera;
+
+    private Transform _mergeTarget;
+
+    private float _zValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //ScreenPointToRay();
+        //WorldToScreenPoint();
+        Merge();
+    }
+
+    // 将二维屏幕的操作转换为三维游戏中的操作  屏幕转世界
+    void ScreenPointToRay()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 hit.transform.GetComponent<MeshRenderer>().material.color = Color.green;
             }
+        }
+    }
+
+    // 世界转屏幕
+    void WorldToScreenPoint()
+    {
+        Vector3 pos = _mainCamera.WorldToScreenPoint(target.position);
+        var mousePosition = Input.mousePosition;
+        mousePosition.z = pos.z;
+        target.position = _mainCamera.ScreenToWorldPoint(mousePosition);
+    }
+
+    void Merge()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                _mergeTarget = hit.transform;
+                _zValue = _mainCamera.WorldToScreenPoint(_mergeTarget.position).z;
+            }
+        } else if (Input.GetMouseButton(0))
+        {
+            if (_mergeTarget is null) return;
+
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = _zValue;
+            _mergeTarget.position = _mainCamera.ScreenToWorldPoint(mousePosition);
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            _mergeTarget = null;
         }
     }
 }
--- Assets/Scripts/InputTest.cs
+++ Assets/Scripts/InputTest.cs
@@ -7,7 +7,7 @@ public class InputTest : MonoBehaviour
     public Transform cube;
 
     public float moveSpeed = 10f;
-    public float rotateSpeed = 10f;
+    public float rotateSpeed = 1f;
 
     private Vector3 _localEulerAnglesOfCube;
 
@@ -22,14 +22,38 @@ public class InputTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //GetAxis();
+        GetButton();
+    }
+
+    // 该系列方法的返回值在[-1,1]之间
+    void GetAxis()
+    {
         // 控制方块的前后左右移动
         cube.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * (Time.deltaTime * moveSpeed));
-        // 控制方块视野左右移动
+        
+        // 控制方块视野随着鼠标左右移动而左右移动(本质上是方块沿y周旋转)
         _localEulerAnglesOfCube.y += Input.GetAxis("Mouse X");
         cube.localEulerAngles = _localEulerAnglesOfCube * rotateSpeed;
 
+        // 控制方块视野随着鼠标上下移动而上下移动(本质上是主相机沿x轴旋转)
         _localEulerAnglesOfCamera.x -= Input.GetAxis("Mouse Y");
         _localEulerAnglesOfCamera.x = Mathf.Clamp(_localEulerAnglesOfCamera.x, -45f, 30f);
         transform.localEulerAngles = _localEulerAnglesOfCamera;
+    }
+
+    // 该系列方法的返回值为bool
+    void GetButton()
+    {
+        if (Input.GetButtonDown("Fire1")) // 点击鼠标左键，向前移动
+        {
+            Debug.Log(Input.mousePosition);
+            cube.Translate(Vector3.forward * (Time.deltaTime * moveSpeed));
+        }
+
+        if (Input.GetButtonDown("Jump")) // 按space按键，向右移动
+        {
+            cube.Translate(Vector3.right * (Time.deltaTime * moveSpeed));
+        }
     }
 }
